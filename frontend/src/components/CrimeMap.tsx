26684bf88e722d2ce961bc7cc94f31542b09ca61
Fortress Lite Developer <dev@fortress-lite.local>
2026-03-13 22:25:21 +0000

    checkpoint: Edit CrimeMap.tsx

--- frontend/src/components/CrimeMap.tsx
+++ frontend/src/components/CrimeMap.tsx
@@ -425,7 +425,7 @@ export default function CrimeMap({ center, zoom, filters, viewMode = 'dots', rat
     l.bindTooltip(tip, { sticky: true });
     l.on('click', (e: any) => {
       console.log('[bindStateInteractions click] sigla=' + sigla);
-      trackEvent('state_click', { state_name: stateName, crime_count: count, view_mode: viewMode });
+      trackEvent('state_click', { state_name: stateName, crime_count: weight, view_mode: viewModeRef.current });
       if (compareModeRef.current && onCompareSelectRef.current) {
         onCompareSelectRef.current({ municipio: '', state: sigla, displayName: `${stateName} (${sigla})` });
         return;
